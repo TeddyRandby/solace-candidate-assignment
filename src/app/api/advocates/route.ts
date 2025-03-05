45b1d070605ba77d24cb651e15b4a2f43d898e25
Teddy Randby <tedrandby@gmail.com>
2025-03-05 06:52:52 -0500

    Fix some bugs and filter on server
    Fixed some bugs preventing page from being usable (Filtering on number,
    missing <tr> element, etc.
    
    Also moved all filtering to take place on the server. The client now
    manages a search term, and on change submits the updated term to the
    server (amoung the query parameters). The server now performs
    case-insensitive matching, and returns the appropriate subset of the
    data.

--- src/app/api/advocates/route.ts
+++ src/app/api/advocates/route.ts
@@ -1,12 +1,40 @@
 import db from "../../../db";
 import { advocates } from "../../../db/schema";
 import { advocateData } from "../../../db/seed/advocates";
+import { NextRequest } from "next/server";
 
-export async function GET() {
+export type Advocate = typeof advocateData[0]
+
+export function GET(req: NextRequest) {
+  const term = req.nextUrl.searchParams.get("term")
   // Uncomment this line to use a database
   // const data = await db.select().from(advocates);
 
-  const data = advocateData;
+  let data = advocateData;
+  console.log(term)
+
+  if (term) {
+    /*
+     * This seems like the cleanest way to perform case-insensitive
+     * search in javascript (aside from converting everything to lower-case first).
+     * A regex is compiled once, and matching is relatively fast. Converting
+     * all strings and search terms to lowercase can be significant overhead.
+     * 
+     * If there was actually a database layer, the case-insensitive portion
+     * could be handled at that level (and that would be way better)
+     */
+    const caseInsensitiveMatch = new RegExp(term, "i")
+
+    data = data.filter((advocate) => {
+      return (
+        advocate.firstName.match(caseInsensitiveMatch) ||
+        advocate.lastName.match(caseInsensitiveMatch) ||
+        advocate.city.match(caseInsensitiveMatch) ||
+        advocate.degree.match(caseInsensitiveMatch) ||
+        advocate.specialties.some((specialty) => specialty.match(caseInsensitiveMatch))
+      );
+    })
+  }
 
   return Response.json({ data });
 }
